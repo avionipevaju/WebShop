77034c3e3619659838702e9fae323e775b0645b3
avionipevaju <antic.nikola@outlook.com>
2017-11-28 00:22:04 +0100

    Registration connection to app's backend

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient, private router: Router) { }
 
   ngOnInit() {
+  }
+
+  registerUser() {
+
+    const usernameField = document.getElementById('username') as HTMLInputElement;
+    const passwordField = document.getElementById('password') as HTMLInputElement;
+    const json = {'username': usernameField.value, 'password': passwordField.value};
+    this.http.post('http://localhost:8080/WebShopDWP/rest/user/register', JSON.stringify(json),
+      {headers: {'Content-Type': 'application/json'}})
+      .subscribe(data => {
+        console.log(data['status']);
+        if (data['status'] === 'OK') {
+          alert('Registration successful');
+          this.router.navigate(['./dashboard']);
+        }
+      });
   }
 
 }
